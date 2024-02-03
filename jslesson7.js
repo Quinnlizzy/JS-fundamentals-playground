5569eb84fde135b25f071bf38349f6d276f1720f
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-03 15:28:41 +0000

    strated

--- jslesson7.js
+++ jslesson7.js
@@ -5,3 +5,8 @@
 //incremebt acccount on click
 
 //chage the html to reflect the new count
+
+
+function increment (){
+    console.log('The button was clicked')
+}
---
+++ jslesson8.js
@@ -0,0 +1,18 @@
+//store the count
+
+//listen for clicks
+
+//incremebt acccount on click
+
+//chage the html to reflect the new count
+
+
+function countdown (){
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+countdown()
