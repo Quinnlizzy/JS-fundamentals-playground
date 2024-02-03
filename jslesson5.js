33367516d82ed8eaf0feb566704561a26da14a15
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-03 15:07:19 +0000

    strated

--- jslesson5.js
+++ jslesson5.js
@@ -1,5 +1,15 @@
 let count = 5
 count = 3
 count = 1
+count = 5
+count = count + 5
 console.log(count)
 
+let bonusPoints = 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
