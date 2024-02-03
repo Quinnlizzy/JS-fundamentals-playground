bc168a2cc2dc868eba9c51ac13f9136f46d13356
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-03 16:55:41 +0000

    strated

---
+++ jelesson14.js
@@ -0,0 +1,9 @@
+//working w the DOM element
+
+// grab the count-el element, store it in a countEl variable
+let count = 0
+let countEl = document.getElementById("count-el")
+function increment() {
+    count = count + 1
+    countEl.innerText=count
+}
