9af037ba7c0d102cba49c3666ad7261d7b1dcd91
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-03 17:21:38 +0000

    strated

---
+++ jslesson18.js
@@ -0,0 +1,21 @@
+let name = 42
+let greeting = "Sup, my name is"
+
+let myGreeting = greeting + ", " + name + "."
+console.log(myGreeting)
+
+//in a match between stroing and int - stroing wins
+
+let points = 4
+let bonusPoints = "10"
+
+let totalPoints = points + bonusPoints
+
+console.log(totalPoints)
+
+// = 410 puts the int and strng together but as a string - again string wins
+
+console.log(4 + 5) //9
+console.log("2" + "4")//"24"
+console.log("5" + 1)//"51"
+console.log(100 + "100")//"100100"
