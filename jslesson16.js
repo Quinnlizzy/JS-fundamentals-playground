1d81aa80f5367bcfc0e00c41c1420fdeb51c4311
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-03 17:09:00 +0000

    strated

---
+++ jslesson16.js
@@ -0,0 +1,11 @@
+let username = "per"
+
+console.log(username)
+
+
+// Create a variable, message, that stores the string: "You have tree new notifications"
+
+
+
+let message = "You have three new notifications"
+console.log(message)
