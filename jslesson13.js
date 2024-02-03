0b9abfe8adf1d6fc0ba5bf2fa9dcc25195eb95bd
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-03 16:18:42 +0000

    strated

---
+++ jslesson13.js
@@ -0,0 +1,14 @@
+// document.getElementById("count").innerText = 5
+
+// change the count-el in the HTML to reflect the new count
+
+let count = 0
+let countEL=document.getElementById("count-el")
+
+console.log(countEL)
+
+function increment() {
+    count = count + 1
+    console.log(count)
+    countEL.innerText = count
+}
