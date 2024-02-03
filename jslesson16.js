204ae78dc150517771692ae0053dd09c2f546b2d
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-03 17:13:39 +0000

    strated

--- jslesson16.js
+++ jslesson16.js
@@ -11,3 +11,7 @@ let message = "You have three new notifications"
 console.log(message)
 
 console.log(message + ", " + username + "!")
+
+// Create a variable, messageToUser, that contains the message we have logged
+let messageToUser = message + ", " + username + "!"
+console.log(messageToUser)
