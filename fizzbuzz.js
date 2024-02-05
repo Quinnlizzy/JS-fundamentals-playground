9281bab423fca8b2e56fcb6a572e80bcccb970ff
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-05 16:10:42 +0000

    record of examples

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
 function fizzBuzz(n) {
-    for(let i = 1; i <= n; i++){
+    for(let i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) console.log('Fizzbuzz');
         else if (i % 3 === 0 && i % 5 !== 0) console.log('Fizz');
         else if (i % 5 === 0 && i % 3 !== 0) console.log('Buzz');
