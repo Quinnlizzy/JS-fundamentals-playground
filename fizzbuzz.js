03209d08930c34dc888aca1c8747a5c00bb9aa8a
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-05 16:08:11 +0000

    record of examples

---
+++ fizzbuzz.js
@@ -0,0 +1,8 @@
+function fizzBuzz(n) {
+    for(let i = 1; i <= n; i++){
+        if (i % 3 === 0 && i % 5 === 0) console.log('Fizzbuzz');
+        else if (i % 3 === 0 && i % 5 !== 0) console.log('Fizz');
+        else if (i % 5 === 0 && i % 3 !== 0) console.log('Buzz');
+        else if (i % 3 !== 0 && i % 5 !== 0) console.log(i);
+    }
+}
