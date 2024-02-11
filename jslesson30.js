edf20c44530afd301dfdbb5eb11913668c17ded7
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-11 21:10:09 +0000

    new lessons

---
+++ jslesson30.js
@@ -0,0 +1,11 @@
+let age = 19
+
+// if less than 21 -> "You can not enter the club!"
+// else            -> "Welcome!"
+
+if (age < 21){
+    console.log ("you can not enter the club")
+}
+else {
+    console.log ("welcome")
+}
