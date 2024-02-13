c544eb2d6faf0052de60f2c13c5ca0ad3ceb889c
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-13 23:00:08 +0000

    tidying errors

--- jslesson34.js
+++ jslesson34.js
@@ -3,7 +3,7 @@ let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 
-if (sum <= 20) {
+if (sum < 21) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
