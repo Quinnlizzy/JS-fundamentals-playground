d63b1f647c9d40b66fad52063c0aec799b45d52c
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-13 22:57:38 +0000

    new lessons

--- jslesson38.js
+++ jslesson38.js
@@ -1,27 +1,27 @@
 // Array - ordered list of items - coposite / complex data type
 
-// Create an array that describes yourself. Use the three primitive data types you've learned
-// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+// // Create an array that describes yourself. Use the three primitive data types you've learned
+// // It should contain your name (string), your age (number), and whether you like pizza (boolean)
 
-let per = ["Per Harald Borgen", 35, true]
+// let per = ["Per Harald Borgen", 35, true]
 
-let cards = [7, 4]
-cards.push(6)
-//console.log(cards)
+// let cards = [7, 4]
+// cards.push(6)
+// //console.log(cards)
 
-// Push the newMessage to the messages array, and then log out the array
+// // Push the newMessage to the messages array, and then log out the array
 
-let messages = [
-    "Hey, how's it going?",        
-    "I'm great, thank you! How about you?",
-    "All good. Been working on my portfolio lately."
-]
+// let messages = [
+//     "Hey, how's it going?",        
+//     "I'm great, thank you! How about you?",
+//     "All good. Been working on my portfolio lately."
+// ]
 
-let newMessage = "Same here!"
+// let newMessage = "Same here!"
 
-messages.push(newMessage)
+// messages.push(newMessage)
 
-console.log(messages)
+// console.log(messages)
 
 let cards = [7, 4]
 cards.push(6)
