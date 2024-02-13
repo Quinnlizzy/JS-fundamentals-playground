e02163587fbd3a430dc415a38adb07aa6f9862cc
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-13 22:57:19 +0000

    new lessons

--- jslesson38.js
+++ jslesson38.js
@@ -5,7 +5,7 @@
 
 let per = ["Per Harald Borgen", 35, true]
 
-// Remove the duplicate declaration of the 'cards' variable
+let cards = [7, 4]
 cards.push(6)
 //console.log(cards)
 
@@ -30,7 +30,7 @@ cards.push(6)
 // Push the newMessage to the messages array, and then log out the array
 
 let messages = [
-    "Hey, how's it going?",        
+    "Hey, how's it going?",
     "I'm great, thank you! How about you?",
     "All good. Been working on my portfolio lately."
 ]
