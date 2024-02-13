7f8ce570a1f47781397a130bcd1c8d406bbcbe39
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-13 23:00:41 +0000

    tidying errors

--- jslesson35.js
+++ jslesson35.js
@@ -3,5 +3,3 @@ console.log(5 > 2)    // true
 console.log(12 > 12)  // false
 console.log(3 < 0)    // false
 console.log(3 >= 3)   // true
-console.log(11 <= 11) // true
-console.log(3 <= 2)   // false
