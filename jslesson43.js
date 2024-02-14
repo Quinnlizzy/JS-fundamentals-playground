1113a229282aef4192f7e57fd639abf59b765c65
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-14 22:08:32 +0000

    continuing

--- jslesson43.js
+++ jslesson43.js
@@ -1,8 +1,17 @@
-let sentence = ["Hello ", "my ", "name ", "is ", "Per"] 
+// let sentence = ["Hello ", "my ", "name ", "is ", "Per"]
+// let greetingEl = document.getElementById("greeting-el")
+
+// // Render the sentence in the greetingEl paragraph using a for loop and .textContent
+
+// for (let i = 0; i < sentence.length; i++) {
+//     greetingEl.textContent += sentence[i]
+// }
+
+let sentence = ["Hello", "my", "name", "is", "Per"] 
 let greetingEl = document.getElementById("greeting-el")
 
-// Render the sentence in the greetingEl paragraph using a for loop and .textContent
+// How do you keep the spaces between the words if I remve them from the array?
 
 for (let i = 0; i < sentence.length; i++) {
-    greetingEl.textContent += sentence[i]
+    greetingEl.textContent += sentence[i] + " "
 }
