1f323f4050a1d7bfb163c877c45b6c76a6752b14
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-14 22:08:42 +0000

    continuing

--- jslesson43.js
+++ jslesson43.js
@@ -7,7 +7,7 @@
 //     greetingEl.textContent += sentence[i]
 // }
 
-let sentence = ["Hello", "my", "name", "is", "Per"] 
+let sentence = ["Hello", "my", "name", "is", "Per"]
 let greetingEl = document.getElementById("greeting-el")
 
 // How do you keep the spaces between the words if I remve them from the array?
