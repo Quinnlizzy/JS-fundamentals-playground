198ece395262a0f1214ed1efa7a8b93d4e1af9c3
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-19 21:20:58 +0000

    more lessons

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,8 @@
+// 2. Create the player object. Give it two keys, name and chips, and set their values
+let player = {
+    name: "Per",
+    chips: 200
+}
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -6,14 +11,10 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-
-let player = {
-    name: "Per",
-    chips: 145
-}
-
-
+// 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
 let playerEl = document.getElementById("player-el")
+
+// 4. Render the player's name and chips in playerEl
 playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
@@ -26,6 +27,7 @@ function getRandomCard() {
         return randomNumber
     }
 }
+
 
 function startGame() {
     isAlive = true
@@ -41,7 +43,6 @@ function renderGame() {
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
-    
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
