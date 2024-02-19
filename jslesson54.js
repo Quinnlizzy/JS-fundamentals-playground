aeb6573bd8e17c46e2c5842d415b007e9de95cb1
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-19 21:08:50 +0000

    more lessons

---
+++ jslesson54.js
@@ -0,0 +1,13 @@
+// Create an object that represents an airbnb castle listing.
+// It should contain at least one boolean, one string, one number, and one array
+// Log out at least two of the keys using the dot notation
+
+let listing = {
+    name: "live like a king in my castle",
+    price: 190 + " per night",
+    features: ["4 beds", "2 guests"],
+    isAvailable: false,
+}
+
+console.log(listing.features[0]);
+
