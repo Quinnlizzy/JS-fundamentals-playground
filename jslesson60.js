347880181c4449fa29952c0335183d43170d8217
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-20 10:36:18 +0000

    furtherlessons

--- jslesson60.js
+++ jslesson60.js
@@ -2,9 +2,9 @@ let hands = ["rock", "paper", "scissor"]
 
 // Create a function that returns a random item from the array
 
-function rps(){
-    let player = Math.floor(Math.random() * 3) 
-    console.log(hands[player]);
+function getHand() {
+    let randomIndex = Math.floor( Math.random() * 3)
+    return hands[randomIndex]
 }
 
-rps()
+console.log( getHand() )
