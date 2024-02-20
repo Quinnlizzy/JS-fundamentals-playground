f4365bcee3024806731fdd2e52a5ad2bb6944b13
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-20 11:19:55 +0000

    furtherlessons

---
+++ emojifighters.js
@@ -0,0 +1,17 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+    // Challenge:
+    // When the user clicks on the "Pick Fighters" button, pick two random 
+    let fighter1 = Math.floor(Math.random() * 16) + 1;
+    
+    let fighter2 = Math.floor(Math.random() * 16) + 1;
+    console.log(fighters[fighter1]);
+    console.log(fighters[fighter2]);
+    
+    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    stageEl.textContent = fighters[fighter1] + " vs " + fighters[fighter2];
+    })
