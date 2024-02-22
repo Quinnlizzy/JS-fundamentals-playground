aaf04b0876fab936ad0688cecd8b2b90c26334a0
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-22 16:46:59 +0000

    furthchromeext

--- chromeextension.js
+++ chromeextension.js
@@ -1,7 +1,5 @@
-//make on click wrk for all the buttons// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
 
-inputbutton = document.getElementById("input-btn");
-
-function buttonClick (){
-    inputbutton.onclick = console.log("Button clicked!")
+function saveLead() {
+    console.log("Button clicked!")
 }
