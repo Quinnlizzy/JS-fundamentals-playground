2c523932b4bb273dd0456219816cf05908c1b2b4
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-23 21:34:14 +0000

    updated&morelessons

--- chromeextension.js
+++ chromeextension.js
@@ -1,6 +1,6 @@
 let inputBtn = document.getElementById("input-btn");
 let myLeads = [];
-let inputEl = document.getElementById("input-el");
+const inputEl = document.getElementById("input-el");
 
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
