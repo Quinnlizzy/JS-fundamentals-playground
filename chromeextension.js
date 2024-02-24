3983d888519a61c2ee8536c6a50f39e7d64e2c9a
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-24 19:16:57 +0000

    newlesson

--- chromeextension.js
+++ chromeextension.js
@@ -8,8 +8,7 @@ inputBtn.addEventListener("click", function() {
     console.log(myLeads)
 })
 
-
+// Render the leads in the unordered list using ulEl.textContent
 for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
+    ulEl.textContent += myLeads[i]
 }
-
