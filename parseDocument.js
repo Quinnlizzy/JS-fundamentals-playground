62e4e7cc80d3b68c9cc901f7025cdb173fe46ba3
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-25 21:27:12 +0000

    furtherlessons

--- parseDocument.js
+++ parseDocument.js
@@ -5,18 +5,18 @@ function parseDocument(document)
     //split the doc into an array of lines
     let lines = documentWithoutLineOne.split(/\r\n|\r|\n/);
     let numLines = lines.length;
-    
+
     //number of characters - just docu .length?
-    
+
     let numCharacters = documentWithoutLineOne.length;
-    
+
     //number of letters
     //all case, a-z match
     let numLetters = documentWithoutLineOne.match(/[a-zA-Z]/g).length;
-    
+
     //upp
     let numUppLetters = documentWithoutLineOne.match(/[A-Z]/g).length;
-    
+
     //low
     let numLowLetters = documentWithoutLineOne.match(/[a-z]/g).length;
 
