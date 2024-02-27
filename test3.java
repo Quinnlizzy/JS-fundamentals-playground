a41442271d15928ae7564acde381699634465554
Quinnlizzy <quin27@hotmail.co.uk>
2024-02-27 11:02:27 +0000

    test

---
+++ test3.java
@@ -0,0 +1,28 @@
+class Example
+{
+
+    public static void main(String[] args)
+    {
+        example(args);
+
+        for (int j=0; j<5; j++)
+        {
+            j--;
+             for (int i=0; i<n; ++i)
+            {
+            System.out.print(i + " "};
+            }
+        }
+    }
+
+    public static String example(String arg)
+    {
+        int t = 10
+        int r = 100;
+
+        //set r to the sum or t & r
+        int r = t * r;
+        return r;
+    }
+}
+        
