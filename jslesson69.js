8d094489b34274f98f8db5377640d64d02d9c6fc
Quinnlizzy <quin27@hotmail.co.uk>
2024-03-22 22:41:04 +0000

    l68

--- jslesson69.js
+++ jslesson69.js
@@ -1,4 +1,10 @@
-let trueOfFalse = Boolean("hello")
+// let trueOfFalse = Boolean("hello")
 
-console.log(trueOfFalse)
+// console.log(trueOfFalse)
 
+console.log(  Boolean("")   ) // false  
+console.log(  Boolean("0")  ) // true
+console.log(  Boolean(100)  ) // true
+console.log(  Boolean(null) ) // false
+console.log(  Boolean([0])  ) // true
+console.log(  Boolean(-0)   ) // false
