cd3b287061cce0bc4a7f62dd002b2a015020f618
Quinnlizzy <quin27@hotmail.co.uk>
2024-03-22 22:35:30 +0000

    l68

---
+++ jslesson68.js
@@ -0,0 +1,10 @@
+let currentViewers = null
+
+currentViewers = ["jane", "nick"]
+
+currentViewers = null
+
+if (currentViewers) {
+    // do something , e.g. notify the live streamers
+    console.log("we have viewers")
+}
