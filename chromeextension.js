702309396e29061944fdda5643e5e2949f1486fb
Quinnlizzy <quin27@hotmail.co.uk>
2024-03-22 22:56:09 +0000

    l68

--- chromeextension.js
+++ chromeextension.js
@@ -38,11 +38,6 @@ function renderLeads() {
 
 
 
-
-
-
-//ll
-
 // 1. Save a key-value pair in localStorage
 // 2. Refresh the page. Get the value and log it to the console
 // 3. Clear localStorage
