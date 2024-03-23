6d82cb05631280efef537352a790245bef93b3f6
Quinnlizzy <quin27@hotmail.co.uk>
2024-03-23 17:46:58 +0000

    tidylessonsup

--- emojifighters.js
+++ emojifighters.js
@@ -5,10 +5,10 @@ let fightButton = document.getElementById("fightButton")
 
 fightButton.addEventListener("click", function() {
     let fighter1 = Math.floor(Math.random() * 16) + 1;
-    
+
     let fighter2 = Math.floor(Math.random() * 16) + 1;
     console.log(fighters[fighter1]);
     console.log(fighters[fighter2]);
-    
+
     stageEl.textContent = fighters[fighter1] + " vs " + fighters[fighter2];
     })
