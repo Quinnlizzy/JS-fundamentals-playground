98580e23c100d75c8b96f148153da115f64d6351
Quinnlizzy <quin27@hotmail.co.uk>
2024-03-25 22:06:28 +0000

    updated extension code for del buttn

--- chromeextension.js
+++ chromeextension.js
@@ -11,29 +11,16 @@ if (leadsFromLocalStorage) {
     renderLeads()
 }
 
-// 2. Listen for double clicks on the delete button (google it!) - 
+// 2. Listen for double clicks on the delete button (google it!)
 // 3. When clicked, clear localStorage, myLeads, and the DOM
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
     renderLeads()
 })
 
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
-}
+
 
 
 
